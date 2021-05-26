8405e5e84c7dd557313879a07053828c9d075af3
Maria Wright <maria@mariawrightstudios.com>
2021-05-26 10:52:28 -0400

    connecting to frontend in development

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ app.use(errorLogger);
 
 app.use(apiErrorHandler);
 
-// // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('frontend/build'));
-//   app.use('*', (req, res) => res.sendFile(path
-//     .resolve(__dirname, 'frontend', 'build', 'index.html')));
-// }
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.use('*', (req, res) => res.sendFile(path
+    .resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
